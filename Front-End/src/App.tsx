72532d226314adf85412302d71954c034a988be3
tmarques2 <thainaramarques258@gmail.com>
2026-01-08 14:03:57 -0300

    Deploy

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -24,7 +24,7 @@ export function App() {
     <>
       {/* Toggle */}
       <div className="position-fixed top-0 end-0 p-3 z-3">
-        <button className="btn btn-primary" onClick={() => setDark(!dark)}>
+        <button className="btn btn-light" onClick={() => setDark(!dark)}>
           {dark ? "Claro" : "Escuro"}
         </button>
       </div>
